136b53dc1f6273564309fe2758c174f38190bb6d
Jose Carrillo <jose@apurata.com>
2020-09-08 11:31:21 -0500

    Update version to 0.1.0
    To install this version, you need to completely remove previous
    versions.

--- woocommerce-apurata-payment-gateway.php
+++ woocommerce-apurata-payment-gateway.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Version:           0.0.12
+ * Version:           0.1.0
  * Plugin Name:       WooCommerce aCuotaz Apurata Payment Gateway
  * Plugin URI:        https://github.com/apurata/woocommerce-apurata-payment-gateway
  * Description:       Finance your purchases with a quick aCuotaz Apurata loan.
